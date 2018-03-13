2f33bc53c39082804aeadf6d9cc4d9883851129d
wangjian <jian.wang@ptmind.com>
2018-03-13 11:17:59 +0800

    fix

--- scheduler-server/src/main/java/com/soft/scheduler/config/SchedulerConfig.java
+++ scheduler-server/src/main/java/com/soft/scheduler/config/SchedulerConfig.java
@@ -1,5 +1,6 @@
 package com.soft.scheduler.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
  * @Author ptmind
  * @Create 2018-03-13
  */
+@Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
     private final int POOL_SIZE = 10;
 
