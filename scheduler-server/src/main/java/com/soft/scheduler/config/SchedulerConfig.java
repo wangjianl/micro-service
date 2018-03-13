57a72d7f82dd0a1e46a92d355ddd62459e1daa49
wangjian <jian.wang@ptmind.com>
2018-03-13 11:16:33 +0800

    添加执行任务的线程池

---
+++ scheduler-server/src/main/java/com/soft/scheduler/config/SchedulerConfig.java
@@ -0,0 +1,22 @@
+package com.soft.scheduler.config;
+
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+/**
+ * @Author ptmind
+ * @Create 2018-03-13
+ */
+public class SchedulerConfig implements SchedulingConfigurer {
+    private final int POOL_SIZE = 10;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler =new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("wj - thread - pool -1");
+        threadPoolTaskScheduler.initialize();
+        scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+}
--- scheduler-server/src/main/java/com/soft/scheduler/task/ScheduledTasks.java
+++ scheduler-server/src/main/java/com/soft/scheduler/task/ScheduledTasks.java
@@ -1,4 +1,4 @@
-package com.soft.scheduler.config;
+package com.soft.scheduler.task;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
