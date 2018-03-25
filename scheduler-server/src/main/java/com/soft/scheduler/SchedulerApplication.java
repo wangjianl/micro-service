72b54aa298d0e515cba339b1254058a7cb3eeda8
wangjian <wangjian@miaohi.com>
2018-03-25 17:36:36 +0800

    fix add

--- scheduler-server/src/main/java/com/soft/scheduler/SchedulerApplication.java
+++ scheduler-server/src/main/java/com/soft/scheduler/SchedulerApplication.java
@@ -1,5 +1,6 @@
 package com.soft.scheduler;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
--- scheduler-server/src/main/java/com/soft/scheduler/config/SchedulerConfig.java
+++ scheduler-server/src/main/java/com/soft/scheduler/config/SchedulerConfig.java
@@ -13,7 +13,6 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 public class SchedulerConfig implements SchedulingConfigurer {
     private final int POOL_SIZE = 10;
 
-    @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
         ThreadPoolTaskScheduler threadPoolTaskScheduler =new ThreadPoolTaskScheduler();
         threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
