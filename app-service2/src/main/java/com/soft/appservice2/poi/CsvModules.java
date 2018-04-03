ba1173a58eb15e585863d8341302a5f1bfe436b8
wangjian <jian.wang@ptmind.com>
2018-04-03 21:12:54 +0800

    fix add

---
+++ app-service2/src/main/java/com/soft/appservice2/poi/CsvModules.java
@@ -0,0 +1,349 @@
+package com.soft.appservice2.poi;
+
+import java.io.Serializable;
+
+/**
+ * @Author ptmind
+ * @Create 2018-03-30
+ */
+public class CsvModules implements Serializable {
+    String A,B,C,D,E,F,G,H,I,J,K,L,M,N,
+            O,P,Q,R,S,T,U,V,W,X,Y,Z,
+            AA,AB,AC,AD,AE,AF,AG,AH,AI,AJ,AK,AL,AM,AN,AO,AP;//
+
+    public String getA() {
+        return A;
+    }
+
+    public void setA(String a) {
+        A = a;
+    }
+
+    public String getB() {
+        return B;
+    }
+
+    public void setB(String b) {
+        B = b;
+    }
+
+    public String getC() {
+        return C;
+    }
+
+    public void setC(String c) {
+        C = c;
+    }
+
+    public String getD() {
+        return D;
+    }
+
+    public void setD(String d) {
+        D = d;
+    }
+
+    public String getE() {
+        return E;
+    }
+
+    public void setE(String e) {
+        E = e;
+    }
+
+    public String getF() {
+        return F;
+    }
+
+    public void setF(String f) {
+        F = f;
+    }
+
+    public String getG() {
+        return G;
+    }
+
+    public void setG(String g) {
+        G = g;
+    }
+
+    public String getH() {
+        return H;
+    }
+
+    public void setH(String h) {
+        H = h;
+    }
+
+    public String getI() {
+        return I;
+    }
+
+    public void setI(String i) {
+        I = i;
+    }
+
+    public String getJ() {
+        return J;
+    }
+
+    public void setJ(String j) {
+        J = j;
+    }
+
+    public String getK() {
+        return K;
+    }
+
+    public void setK(String k) {
+        K = k;
+    }
+
+    public String getL() {
+        return L;
+    }
+
+    public void setL(String l) {
+        L = l;
+    }
+
+    public String getM() {
+        return M;
+    }
+
+    public void setM(String m) {
+        M = m;
+    }
+
+    public String getN() {
+        return N;
+    }
+
+    public void setN(String n) {
+        N = n;
+    }
+
+    public String getO() {
+        return O;
+    }
+
+    public void setO(String o) {
+        O = o;
+    }
+
+    public String getP() {
+        return P;
+    }
+
+    public void setP(String p) {
+        P = p;
+    }
+
+    public String getQ() {
+        return Q;
+    }
+
+    public void setQ(String q) {
+        Q = q;
+    }
+
+    public String getR() {
+        return R;
+    }
+
+    public void setR(String r) {
+        R = r;
+    }
+
+    public String getS() {
+        return S;
+    }
+
+    public void setS(String s) {
+        S = s;
+    }
+
+    public String getT() {
+        return T;
+    }
+
+    public void setT(String t) {
+        T = t;
+    }
+
+    public String getU() {
+        return U;
+    }
+
+    public void setU(String u) {
+        U = u;
+    }
+
+    public String getV() {
+        return V;
+    }
+
+    public void setV(String v) {
+        V = v;
+    }
+
+    public String getW() {
+        return W;
+    }
+
+    public void setW(String w) {
+        W = w;
+    }
+
+    public String getX() {
+        return X;
+    }
+
+    public void setX(String x) {
+        X = x;
+    }
+
+    public String getY() {
+        return Y;
+    }
+
+    public void setY(String y) {
+        Y = y;
+    }
+
+    public String getZ() {
+        return Z;
+    }
+
+    public void setZ(String z) {
+        Z = z;
+    }
+
+    public String getAA() {
+        return AA;
+    }
+
+    public void setAA(String AA) {
+        this.AA = AA;
+    }
+
+    public String getAB() {
+        return AB;
+    }
+
+    public void setAB(String AB) {
+        this.AB = AB;
+    }
+
+    public String getAC() {
+        return AC;
+    }
+
+    public void setAC(String AC) {
+        this.AC = AC;
+    }
+
+    public String getAD() {
+        return AD;
+    }
+
+    public void setAD(String AD) {
+        this.AD = AD;
+    }
+
+    public String getAE() {
+        return AE;
+    }
+
+    public void setAE(String AE) {
+        this.AE = AE;
+    }
+
+    public String getAF() {
+        return AF;
+    }
+
+    public void setAF(String AF) {
+        this.AF = AF;
+    }
+
+    public String getAG() {
+        return AG;
+    }
+
+    public void setAG(String AG) {
+        this.AG = AG;
+    }
+
+    public String getAH() {
+        return AH;
+    }
+
+    public void setAH(String AH) {
+        this.AH = AH;
+    }
+
+    public String getAI() {
+        return AI;
+    }
+
+    public void setAI(String AI) {
+        this.AI = AI;
+    }
+
+    public String getAJ() {
+        return AJ;
+    }
+
+    public void setAJ(String AJ) {
+        this.AJ = AJ;
+    }
+
+    public String getAK() {
+        return AK;
+    }
+
+    public void setAK(String AK) {
+        this.AK = AK;
+    }
+
+    public String getAL() {
+        return AL;
+    }
+
+    public void setAL(String AL) {
+        this.AL = AL;
+    }
+
+    public String getAM() {
+        return AM;
+    }
+
+    public void setAM(String AM) {
+        this.AM = AM;
+    }
+
+    public String getAN() {
+        return AN;
+    }
+
+    public void setAN(String AN) {
+        this.AN = AN;
+    }
+
+    public String getAO() {
+        return AO;
+    }
+
+    public void setAO(String AO) {
+        this.AO = AO;
+    }
+
+    public String getAP() {
+        return AP;
+    }
+
+    public void setAP(String AP) {
+        this.AP = AP;
+    }
+}
