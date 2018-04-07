d4141a57ceb01994c8fb99713e3ea8f834450226
wangjian <wangjian@miaohi.com>
2018-04-07 12:55:25 +0800

    fix add spring batch csv test

---
+++ app-service2/src/main/java/com/soft/appservice2/poi/CsvServenModules.java
@@ -0,0 +1,109 @@
+package com.soft.appservice2.poi;
+
+import java.io.Serializable;
+
+/**
+ * @Author ptmind
+ * @Create 2018-03-30
+ */
+public class CsvServenModules implements Serializable {
+    String A,B,C,D,E,F,G,H,I,J,K,L;//
+
+    public String getA() {
+        return A;
+    }
+
+    public void setA(String a) {
+        A = a;
+    }
+
+    public String getB() {
+        return B;
+    }
+
+    public void setB(String b) {
+        B = b;
+    }
+
+    public String getC() {
+        return C;
+    }
+
+    public void setC(String c) {
+        C = c;
+    }
+
+    public String getD() {
+        return D;
+    }
+
+    public void setD(String d) {
+        D = d;
+    }
+
+    public String getE() {
+        return E;
+    }
+
+    public void setE(String e) {
+        E = e;
+    }
+
+    public String getF() {
+        return F;
+    }
+
+    public void setF(String f) {
+        F = f;
+    }
+
+    public String getG() {
+        return G;
+    }
+
+    public void setG(String g) {
+        G = g;
+    }
+
+    public String getH() {
+        return H;
+    }
+
+    public void setH(String h) {
+        H = h;
+    }
+
+    public String getI() {
+        return I;
+    }
+
+    public void setI(String i) {
+        I = i;
+    }
+
+    public String getJ() {
+        return J;
+    }
+
+    public void setJ(String j) {
+        J = j;
+    }
+
+    public String getK() {
+        return K;
+    }
+
+    public void setK(String k) {
+        K = k;
+    }
+
+    public String getL() {
+        return L;
+    }
+
+    public void setL(String l) {
+        L = l;
+    }
+
+
+}
