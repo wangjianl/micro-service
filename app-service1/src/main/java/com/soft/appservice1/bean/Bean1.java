744ca96e10f7380e00a730ca30765deea16722ac
wangjian <wangjian@miaohi.com>
2019-01-13 15:10:21 +0800

    fix 20190113

---
+++ app-service1/src/main/java/com/soft/appservice1/bean/Bean1.java
@@ -0,0 +1,43 @@
+package com.soft.appservice1.bean;
+/**
+ * Created by wangjian on 18/11/5.
+ */
+
+import com.google.common.collect.Lists;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ *
+ */
+public class Bean1 {
+
+    public static void main(String []args){
+        String []star = new String [] {"1","2"};
+        System.out.print(Lists.newArrayList("1","2").add("3"));
+        System.out.print(Arrays.asList(star).add("4"));
+
+    }
+
+
+    private Boolean success;
+    private Boolean isFail;
+
+    public Boolean getFail() {
+        Objects.equals("","");
+        return isFail;
+    }
+
+    public void setFail(Boolean fail) {
+        isFail = fail;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+}
--- scheduler-server/src/main/java/com/soft/scheduler/SchedulerApplication.java
+++ scheduler-server/src/main/java/com/soft/scheduler/SchedulerApplication.java
@@ -4,11 +4,12 @@ package com.soft.scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-//@EnableEurekaClient
+@EnableEurekaClient
 public class SchedulerApplication {
 
 	public static void main(String[] args) {
